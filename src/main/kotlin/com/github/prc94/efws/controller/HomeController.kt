54c9d6c759e8d479ef56527ff81deb0906aaa19f
prc94 <thsigma94@yandex.ru>
2023-05-25 01:47:43 +0300

    Added HomeController and index.html

---
+++ src/main/kotlin/com/github/prc94/efws/controller/HomeController.kt
@@ -0,0 +1,14 @@
+package com.github.prc94.efws.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/")
+class HomeController {
+
+    @GetMapping
+    fun index() =
+        "index"
+}
